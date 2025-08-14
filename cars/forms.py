c09f1d650523f791ae441a285897e94ee8a6f7ae
Roberto F. Rodrigues <robertofr_@hotmail.com>
2025-08-14 15:16:35 -0300

    Primeiro commit do projeto Carros

---
+++ cars/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from cars.models import Brand
+
+
+class CarForm(forms.Form):
+    model = forms.CharField(max_length=200)
+    brand = forms.ModelChoiceField(Brand.objects.all())
+    factory_year = forms.IntegerField()
+    model_year = forms.IntegerField()
+    plate = forms.CharField()
+    value = forms.FloatField()
+    photo = forms.ImageField()
+    
